a50674445fde17757b69c005a6272bcb9e6939f5
CodeSandbox <support@codesandbox.io>
2023-02-03 21:46:09 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import deno from "@astrojs/deno";
+
+import netlify from "@astrojs/netlify/edge-functions";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+});
